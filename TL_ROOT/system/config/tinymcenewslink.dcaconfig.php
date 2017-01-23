a301535d04a824d46e7b5e8c45dd863873875fbb
markocupic <m.cupic@gmx.ch>
2017-01-23 12:45:58 +0100

    added tinyNewsCustom.php for tl_news support

--- TL_ROOT/system/config/tinymcenewslink.dcaconfig.php
+++ TL_ROOT/system/config/tinymcenewslink.dcaconfig.php
@@ -1,4 +1,7 @@
 <?php
 
 // Put your custom configuration here
+// Enable custom configuration for tinyMCE rte
 $GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['rte'] = 'tinyCustom';
+$GLOBALS['TL_DCA']['tl_news']['fields']['teaser']['eval']['rte'] = 'tinyNewsCustom';
+$GLOBALS['TL_DCA']['tl_gmk_mitarbeiter']['fields']['publications']['eval']['rte'] = 'tinyCustom';
