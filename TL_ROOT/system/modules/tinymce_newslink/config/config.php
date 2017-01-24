9d64170f69270ca2906229a22e359a0fc5a68b98
Marko Cupic <m.cupic@gmx.ch>
2017-01-24 14:33:31 +0100

    adopted config.php

--- TL_ROOT/system/modules/tinymce_newslink/config/config.php
+++ TL_ROOT/system/modules/tinymce_newslink/config/config.php
@@ -7,16 +7,21 @@
  * @license LGPL-3.0+
  */
 
-
+// This plugin requires https://github.com/markocupic/tinymce_plugin_builder
 if ($GLOBALS['TL_CONFIG']['useRTE'])
 {
-
+    // Add stylesheet
     $GLOBALS['TL_CSS'][] = 'system/modules/tinymce_newslink/assets/css/newslink.css';
 
-    $GLOBALS['TINYMCE']['SETTINGS']['TOOLBAR'][] = 'newslink';
+    // Add a plugin to the tinymce editor
     $GLOBALS['TINYMCE']['SETTINGS']['PLUGINS'][] = 'newslink';
-    $GLOBALS['TINYMCE']['SETTINGS']['CONFIG_ROW']['test_string'] = "'This is a test string, and you have to quote it with a single quote.'";
-    $GLOBALS['TINYMCE']['SETTINGS']['CONFIG_ROW']['newslink_news_data'] = json_encode(TinymceNewslink\TinymceNewslink::getContaoNewsArchivesAsJSON());
-    $GLOBALS['TINYMCE']['SETTINGS']['CONFIG_ROW']['newslink_language_data'] = json_encode($GLOBALS['TL_LANG']['TINYMCE']['NEWSLINK']);
 
+    // Add a button to the toolbar in tinymce editor
+    $GLOBALS['TINYMCE']['SETTINGS']['TOOLBAR'][] = 'newslink';
+
+    // Add a new config row to the tinymce.init method (json_encoded array from a PHP class)
+    $GLOBALS['TINYMCE']['SETTINGS']['CONFIG_ROW']['newslink_news_data'] = json_encode(TinymceNewslink\TinymceNewslink::getContaoNewsArchivesAsJSON());
+
+    // Add a new config row to the tinymce.init method (json_encoded array from a language file)
+    $GLOBALS['TINYMCE']['SETTINGS']['CONFIG_ROW']['newslink_language_data'] = json_encode($GLOBALS['TL_LANG']['TINYMCE']['NEWSLINK']);
 }
